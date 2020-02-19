e100d9c9847a5236cd175e9036fdefc08bcd4360
Michael Ch (DevExpress) <45622253+MichaelDX@users.noreply.github.com>
2020-02-19 18:02:52 +0300

    update

--- CS/dxSampleGrid/App.xaml.cs
+++ CS/dxSampleGrid/App.xaml.cs
@@ -1,14 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Windows;
+﻿using System.Windows;
 
 namespace dxSampleGrid {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application {
-    }
+    public partial class App : Application { }
 }
--- CS/dxSampleGrid/MainWindow.xaml.cs
+++ CS/dxSampleGrid/MainWindow.xaml.cs
@@ -1,34 +1,9 @@
-﻿using DevExpress.Xpf.Grid;
-
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Markup;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
-using System.Windows.Threading;
-using DevExpress.Xpf.Core.ServerMode;
-using System.Data;
+﻿using DevExpress.Xpf.Core;
 
 namespace dxSampleGrid {
-    public partial class MainWindow : Window {
+    public partial class MainWindow : ThemedWindow {
         public MainWindow() {
             InitializeComponent();
-            vm = new MyViewModel();
-            DataContext = vm;
-
         }
-        MyViewModel vm;
     }
 }
